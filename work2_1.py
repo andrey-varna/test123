36dd47aeca4e3d9bd7a4b5184fd4320246e9ace7
andrey-varna <68548513+andrey-varna@users.noreply.github.com>
2020-08-03 23:40:29 +0300

    Задание 4: home1 и work2_1 рабочие

--- work2_1.py
+++ work2_1.py
@@ -1,21 +1,17 @@
 # -*- coding: utf-8 -*-
-import unittest
+import pytest
 from address import Address
 from application1 import Application
 
 
-class UntitledTestCase(unittest.TestCase):
-    def setUp(self):
-        self.app = Application()
-
-    def test_untitled_test_case(self):
-        self.app.login(username="admin", password="secret")
-        self.app.create_address(Address("Viktor", "Prihodko", "08998899854"))
-        self.app.logout()
-
-    def tearDown(self):
-        self.app.destroy()
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
 
-if __name__ == "__main__":
-    unittest.main()
+def test_untitled_test_case(app):
+    app.login(username="admin", password="secret")
+    app.create_address(Address("Viktor", "Prihodko", "08998899854"))
+    app.logout()
