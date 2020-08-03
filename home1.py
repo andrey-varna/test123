127ec46ed19d68352357cede8e0c4fc842033a66
andrey-varna <68548513+andrey-varna@users.noreply.github.com>
2020-08-03 17:58:19 +0300

    Задание 4 подготовка

--- home1.py
+++ home1.py
@@ -1,71 +1,25 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
 import unittest
 from group import Group
+from application import Application
+
 
 class UntitledTestCase(unittest.TestCase):
     def setUp(self):
-        self.wd = webdriver.Chrome()
-        self.wd.implicitly_wait(30)
+        self.app = Application()
 
     def test_add_group(self):
-        wd = self.wd
-        self.open_home_page(wd)
-        self.login(wd, username="admin", password="secret")
-        self.open_groups_page(wd)
-        self.create_group(wd, Group(name="123", header="aSFASEDFHG", footer="AFWR"))
-        self.return_to_groups_page(wd)
-        self.logout(wd)
-
+        self.app.login(username="admin", password="secret")
+        self.app.create_group(Group(name="123", header="aSFASEDFHG", footer="AFWR"))
+        self.app.logout()
 
     def test_add_ampty_group(self):
-        wd = self.wd
-        self.open_home_page(wd)
-        self.login(wd, username="admin", password="secret")
-        self.open_groups_page(wd)
-        self.create_group(wd, Group(name="", header="", footer=""))
-        self.return_to_groups_page(wd)
-        self.logout(wd)
-
-
-    def logout(self, wd):
-        wd.find_element_by_link_text("Logout").click()
-
-    def return_to_groups_page(self, wd):
-        wd.find_element_by_link_text("group page").click()
-
-    def create_group(self, wd, group):
-        # init group creatian
-        wd.find_element_by_name("group_name").click()
-        # fill group firm
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("%s" % group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("%s" % group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("%s" % group.footer)
-        # submit group creation
-        wd.find_element_by_name("submit").click()
-
-    def open_groups_page(self, wd):
-        wd.find_element_by_name("new").click()
-
-    def login(self, wd, username, password):
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("%s" % password)
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("%s" % username)
-        wd.find_element_by_id("LoginForm").click()
-        wd.find_element_by_xpath("//input[@value='Login']").click()
-
-    def open_home_page(self, wd):
-        wd.get("http://localhost/addressbook/group.php")
+        self.app.login(username="admin", password="secret")
+        self.app.create_group(Group(name="", header="", footer=""))
+        self.app.logout()
 
     def tearDown(self):
-        self.wd.quit()
+        self.app.destroy()
 
 
 if __name__ == "__main__":
--- work1_3.py
+++ work1_3.py
@@ -1,61 +1,21 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
 import unittest
 from address import Address
+from application1 import Application
+
 
 class UntitledTestCase(unittest.TestCase):
     def setUp(self):
-        self.wd = webdriver.Chrome()
-        self.wd.implicitly_wait(30)
+        self.app = Application()
 
     def test_untitled_test_case(self):
-        wd = self.wd
-        self.open_home_page(wd)
-        self.login(wd, username="admin", password="secret")
-        self.create_new_address(wd)
-        self.create_address(wd, Address("Viktor", "Prihodko", "08998899854"))
-        self.retern_home_page(wd)
-        self.logout(wd)
+        self.app.login(username="admin", password="secret")
+        self.app.create_address(Address("Viktor", "Prihodko", "08998899854"))
+        self.app.logout()
 
-    def create_address(self, wd, Address):
-        # add address
-        wd.find_element_by_name("firstname").clear()
-        # fill address firm
-        wd.find_element_by_name("firstname").send_keys(Address.name)
-        wd.find_element_by_name("nickname").click()
-        wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys(Address.lastname)
-        wd.find_element_by_name("mobile").click()
-        wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys(Address.phonenumber)
-        # submit address creation
-        wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
-
-    def create_new_address(self, wd):
-        wd.find_element_by_name("firstname").click()
-
-    def retern_home_page(self, wd):
-        wd.find_element_by_link_text("home page").click()
-
-    def logout(self, wd):
-        wd.find_element_by_link_text("Logout").click()
-
-    def login(self, wd, username, password):
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_id("LoginForm").click()
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_id("content").click()
-        wd.find_element_by_xpath("//input[@value='Login']").click()
-
-    def open_home_page(self, wd):
-        wd.get("http://localhost/addressbook/edit.php")
 
     def tearDown(self):
-        self.wd.quit()
+        self.app.destroy()
 
 
 if __name__ == "__main__":
