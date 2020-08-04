50e2352c6e5ec82851e4ffe0714c9ef8cd62be8f
andrey-varna <68548513+andrey-varna@users.noreply.github.com>
2020-08-04 13:52:47 +0300

    Выделен класс для работы с группами

--- test/home2.py
+++ test/home2.py
@@ -13,11 +13,11 @@ def app(request):
 
 def test_add_group(app):
     app.session.login(username="admin", password="secret")
-    app.create_group(Group(name="123", header="aSFASEDFHG", footer="AFWR"))
+    app.group.create(Group(name="123", header="aSFASEDFHG", footer="AFWR"))
     app.session.logout()
 
 
 def test_add_ampty_group(app):
     app.session.login(username="admin", password="secret")
-    app.create_group(Group(name="", header="", footer=""))
+    app.group.create(Group(name="", header="", footer=""))
     app.session.logout()
