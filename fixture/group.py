59f7461061d3545d132279365a058c327fc79041
andrey-varna <68548513+andrey-varna@users.noreply.github.com>
2020-08-05 20:39:38 +0300

    Общая текстура

--- fixture/group.py
+++ fixture/group.py
@@ -12,6 +12,7 @@ class GroupHelper:
     def create(self, group):
         wd = self.app.wd
         self.open_groups_page()
+        wd.find_element_by_name("new").click()
         # init group creatian
         wd.find_element_by_name("group_name").click()
         # fill group firm
@@ -27,6 +28,17 @@ class GroupHelper:
         wd.find_element_by_name("submit").click()
         self.return_to_groups_page()
 
+
+    def delete_first_group(self):
+        wd = self.app.wd
+        self.open_groups_page()
+        # select first group
+        wd.find_element_by_name("selected[]").click()
+        # submit deletion
+        wd.find_element_by_name("delete").click()
+        self.return_to_groups_page()
+
+
     def open_groups_page(self):
         wd = self.app.wd
-        wd.find_element_by_name("new").click()
+        wd.find_element_by_link_text("groups").click()
