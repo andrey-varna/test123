e888b5c240c0238752d5419fd60d49ce16c721b2
andrey-varna <68548513+andrey-varna@users.noreply.github.com>
2020-08-06 14:30:15 +0300

    Добавлен тест удаления контактов

--- fixture/kontakt.py
+++ fixture/kontakt.py
@@ -7,8 +7,6 @@ class KontaktHelper:
     def create_address(self, Address):
         wd = self.app.wd
         self.create_new_address()
-        # add address
-
         # fill address firm
         wd.find_element_by_name("firstname").clear()
         wd.find_element_by_name("firstname").send_keys(Address.name)
@@ -21,6 +19,15 @@ class KontaktHelper:
         # submit address creation
         wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
         self.retern_home_page()
+
+    def del_kontakt(self):
+        wd = self.app.wd
+        # select first kontakt
+        wd.find_element_by_name("selected[]").click()
+        # submit deletion
+        self.accept_next_alert = True
+        wd.find_element_by_xpath("//input[@value='Delete']").click()
+        wd.switch_to_alert().accept()
 
     def create_new_address(self):
         wd = self.app.wd
