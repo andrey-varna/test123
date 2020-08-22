1b621a14cd33da1e43be0ebd3c9506c07d2e00e9
andrey-varna <68548513+andrey-varna@users.noreply.github.com>
2020-08-22 23:57:34 +0300

    Д.з.9 - выполнение предусловий

--- test/tect_modify_kontakt.py
+++ test/tect_modify_kontakt.py
@@ -2,13 +2,19 @@ from model.address import Address
 
 
 def test_modify_kontakt_name(app):
+    if app.kontakt.count() == 0:
+        app.kontakt.create_address(Address(firstname="test"))
     app.kontakt.modify_first_kontakt(Address(firstname="New kontakt"))
 
 
-def test_modify_group_lastname(app):
+def test_modify_kontakt_lastname(app):
+    if app.kontakt.count() == 0:
+        app.kontakt.create_address(Address(firstname="test"))
     app.kontakt.modify_first_kontakt(Address(lastname="New lastname"))
 
 
-def test_modify_group_phonenumber(app):
+def test_modify_kontakt_phonenumber(app):
+    if app.kontakt.count() == 0:
+        app.kontakt.create_address(Address(firstname="test"))
     app.kontakt.modify_first_kontakt(Address(mobile="New phonenumber"))
 
--- test/test_del_kontakt.py
+++ test/test_del_kontakt.py
@@ -1,4 +1,7 @@
+from model.address import Address
 
 
 def test_del_kontakt(app):
+    if app.kontakt.count() == 0:
+        app.kontakt.create_address(Address(firstname="test"))
     app.kontakt.del_kontakt()
