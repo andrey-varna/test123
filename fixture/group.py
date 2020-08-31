0036269fa2f0ebde8b439d84aeac207f8e142254
andrey-varna <68548513+andrey-varna@users.noreply.github.com>
2020-08-31 23:55:07 +0300

    Задание 12, кеширование и хеширование для групп.

--- fixture/group.py
+++ fixture/group.py
@@ -14,6 +14,7 @@ class GroupHelper:
         # submit group creation
         wd.find_element_by_name("submit").click()
         self.return_to_groups_page()
+        self.group_cash = None
 
     def fill_group_form(self, group):
         wd = self.app.wd
@@ -28,6 +29,7 @@ class GroupHelper:
         # submit deletion
         wd.find_element_by_name("delete").click()
         self.return_to_groups_page()
+        self.group_cash = None
 
     def edit_group(self):
         wd = self.app.wd
@@ -40,6 +42,7 @@ class GroupHelper:
         wd.find_element_by_name("group_name").send_keys("edit group")
         wd.find_element_by_name("update").click()
         self.return_to_groups_page()
+        self.group_cash = None
 
     def modify_first_group(self, new_group_date):
         wd = self.app.wd
@@ -52,6 +55,7 @@ class GroupHelper:
         # submit modification
         wd.find_element_by_name("update").click()
         self.return_to_groups_page()
+        self.group_cash = None
 
     def open_groups_page(self):
         wd = self.app.wd
@@ -78,12 +82,15 @@ class GroupHelper:
         self.open_groups_page()
         return len(wd.find_elements_by_name("selected[]"))
 
+    group_cash = None
+
     def get_group_list(self):
-        wd = self.app.wd
-        self.open_groups_page()
-        groups = []
-        for element in wd.find_elements_by_css_selector("span.group"):
-            text = element.text
-            id = element.find_element_by_name("selected[]").get_attribute('value')
-            groups.append(Group(name=text, id=id))
-        return groups
+        if self.group_cash is None:
+            wd = self.app.wd
+            self.open_groups_page()
+            self.group_cash = []
+            for element in wd.find_elements_by_css_selector("span.group"):
+                text = element.text
+                id = element.find_element_by_name("selected[]").get_attribute('value')
+                self.group_cash.append(Group(name=text, id=id))
+        return list(self.group_cash)
